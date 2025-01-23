0f842017684990681140359e52a948e17ef89aaf
Joe Gunter <jgunter@bluecanyontech.com>
2025-01-23 15:02:40 -0700

    include lachsalter's checksum and alternate writing style changes, include globalObject this in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var path = require('path');
 module.exports = {
     entry: './src/TLEValidator.js',
     output: {
+        globalObject: "this",
         path: path.resolve(__dirname, 'dist'),
         filename: 'tle-validator.js',
         library: 'TLEValidator',
